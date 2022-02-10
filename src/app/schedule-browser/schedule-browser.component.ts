4d0aa7ff10cddcd8090af25083197e59b4d688d4
Mike Newton <mike.newton@se.com>
2022-02-10 14:26:41 -0800

    Superbowl spread

--- src/app/schedule-browser/schedule-browser.component.ts
+++ src/app/schedule-browser/schedule-browser.component.ts
@@ -10,7 +10,7 @@ import { RecordService } from 'src/shared/services/record.service';
 })
 export class ScheduleBrowserComponent implements OnInit {
   // schedule: Schedule = null;
-  maxWeek = 20;
+  maxWeek = 22;
   selectedWeekId = 1;
   selectedWeek;
   selectedWeekIndex(): number { return this.selectedWeekId - 1; }
