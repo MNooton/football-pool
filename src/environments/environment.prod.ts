d87b8c48d6bc9c2033bad7511438ca7fd7bf55e2
MIke Newton <mike.newton.blammo@gmail.com>
2025-08-26 20:58:23 -0700

    debugging

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   s3: {
     S3: {
-        bucket: 'arn:aws:s3:::footballappdata', // REQUIRED -  Amazon S3 bucket
+        bucket: 'footballappdata', // REQUIRED -  Amazon S3 bucket
         region: 'us-west-2', // OPTIONAL -  Amazon service region
     }
   }
