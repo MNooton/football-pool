dd88f3f365380c4008b3f5bc6159a674154dc9e2
MIke Newton <mike.newton.blammo@gmail.com>
2025-08-26 20:22:42 -0700

    ng 20 build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,7 @@
 export const environment = {
   production: true,
-  cognito: {
-      identityPoolId: 'us-west-2:38573e23-157a-4097-bd61-42a89029b8ce',
-    userPoolId: 'us-west-2_BGgWBupjc', // 'us-east-1_GuwbXcMjJ',
-    userPoolClientId: '7bp84fv75sp0d0fi2g6cc5t45u', // 'sjo7m2jn0085jne07shsjldvv', // 'u9ag63dicaas31345it4gb1d0',
-    region: 'us-west-2'
-  },
   s3: {
-    AWSS3: {
+    S3: {
         bucket: 'arn:aws:s3:::footballappdata', // REQUIRED -  Amazon S3 bucket
         region: 'us-west-2', // OPTIONAL -  Amazon service region
     }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,6 @@
 
 export const environment = {
   production: false,
-  Auth: { 
-    Cognito: {
-      identityPoolId: 'us-west-2:38573e23-157a-4097-bd61-42a89029b8ce',
-      userPoolId: 'us-west-2_BGgWBupjc',
-      userPoolClientId: '7bp84fv75sp0d0fi2g6cc5t45u', // <-- Note the 'Web' here
-      region: 'us-west-2'
-    }
-  }, 
   s3: {
     S3: { // ✅ not AWSS3
       bucket: 'footballappdata', // ✅ no arn
